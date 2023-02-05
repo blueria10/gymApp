1d50b7b8bcff4ebed24f4e09c9a333b93dd2ec57
Emmanuella Desruisseaux <85198940+blueria10@users.noreply.github.com>
2023-02-04 20:06:37 -0500

    Update gym.py

--- gym.py
+++ gym.py
@@ -7,6 +7,7 @@ from flask import request
 from flask_session import Session
 from flask import render_template
 from waitress import serve
+import gym
 
 app = Flask(__name__)
 app.secret_key = 'the awesome super secret key'
@@ -61,7 +62,7 @@ def chosenofresult():
 
 if __name__ == "__main__":
     # app.run(host="127.0.0.1", port=8000))
-    serve(app, host='127.0.0.1', port=8000,url_prefix="/gymApp",url_scheme='https')
+    serve(gym, host='127.0.0.1', port=8000,url_prefix="/gymApp",url_scheme='https')
     with app.test_request_context("/"):
         session["key"] = "value"
 
